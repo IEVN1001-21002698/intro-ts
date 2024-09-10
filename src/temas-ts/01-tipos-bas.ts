ee805cfb35318f1c44ba9aa86864271d7bd9f93a
Omar Avelar Nachez <raven.reaper0601@gmail.com>
2024-09-09 21:06:36 -0600

    primer commit de ts lunes 9

---
+++ src/temas-ts/01-tipos-bas.ts
@@ -0,0 +1,28 @@
+
+//console.log('hola mundo')
+
+var nombre ='juan'
+let apellido='perez'
+const edad =30
+
+let num1=2
+let num2=5
+let num3=3
+
+let suma = num1+num2+num3
+console.log(suma)
+
+let correo:string;
+let edad2:number;
+let activo:boolean;
+
+correo='xx@gmail.com'
+edad2=22
+activo=true
+
+let matricula:number|string;
+
+matricula=12345
+matricula='23AD45'
+
+
---
+++ src/temas-ts/02-interfaz-obj.ts
@@ -0,0 +1,31 @@
+interface Alumno{
+    nombre:string;
+    edad:number;
+    apellido:string;
+    genero:string;
+    calificacion?:number;
+}
+
+const alumno:Alumno={
+    nombre:"mario",
+    apellido:"lopez",
+    edad:23,
+    genero:"M",
+}
+
+console.table(alumno)
+
+let mascotas=['perico','perro','gato'];
+
+console.log(mascotas[1]);
+mascotas[1]='nuevo perro';
+console.log(mascotas[1]);
+mascotas.push('hamster');
+console.log(mascotas);
+
+let tem:(string|number)[]=[];
+
+tem.push(11);
+tem.push('mario')
+
+console.log(tem)
